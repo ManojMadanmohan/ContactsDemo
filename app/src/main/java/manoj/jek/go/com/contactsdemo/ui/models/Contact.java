f3a7e82cd87648265d3ef0b50c160b477fd56461
Manoj <manoj@zophop.com>
2016-08-05 00:54:10 +0530

    model change to include favorite status

--- app/src/main/java/manoj/jek/go/com/contactsdemo/ui/models/Contact.java
+++ app/src/main/java/manoj/jek/go/com/contactsdemo/ui/models/Contact.java
@@ -8,15 +8,17 @@ public class Contact {
     private String _profile_pic;
     private String email;
     private int id;
+    private boolean _isFavorite;
 
     public Contact(int id, String firstName, String lastName, String email,
-                   String number, String profilePicUrl) {
+                   String number, String profilePicUrl, boolean isFav) {
         this.email = email;
         this.id = id;
         first_name = firstName;
         last_name = lastName;
         this.phone_number = number;
         _profile_pic = profilePicUrl;
+        _isFavorite = isFav;
     }
 
     public String getFirstName() {
@@ -37,6 +39,14 @@ public class Contact {
 
     public String getEmail() {
         return email;
+    }
+
+    public void makeFavorite() {
+        _isFavorite = true;
+    }
+
+    public boolean  getIsFavorite() {
+        return _isFavorite;
     }
 
     public int getId() {
