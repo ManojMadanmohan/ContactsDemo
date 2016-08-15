c7ff5b48825f5254965e0999bc36a8b1784b5a95
Manoj <manoj@zophop.com>
2016-08-15 20:48:22 +0530

    actionbar

--- app/src/main/java/manoj/jek/go/com/contactsdemo/ui/activities/NewContactActivity.java
+++ app/src/main/java/manoj/jek/go/com/contactsdemo/ui/activities/NewContactActivity.java
@@ -14,6 +14,7 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -77,6 +78,13 @@ public class NewContactActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_contact);
         ButterKnife.bind(this);
         setListeners();
+        setActionBar();
+    }
+
+    private void setActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private void setListeners(){
