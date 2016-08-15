af55844b3ef4993d088d2828b6ede52a8a2502e3
Manoj <manoj@zophop.com>
2016-08-15 18:19:12 +0530

    new activity to add contacts

---
+++ app/src/main/java/manoj/jek/go/com/contactsdemo/ui/activities/NewContactActivity.java
@@ -0,0 +1,15 @@
+package manoj.jek.go.com.contactsdemo.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import manoj.jek.go.com.contactsdemo.R;
+
+public class NewContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_contact);
+    }
+}
