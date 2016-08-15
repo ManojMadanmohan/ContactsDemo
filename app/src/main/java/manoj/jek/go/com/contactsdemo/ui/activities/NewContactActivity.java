2e100e31784b1378a42925efa16e71d96909f747
Manoj <manoj@zophop.com>
2016-08-15 19:00:20 +0530

    implementation for image

--- app/src/main/java/manoj/jek/go/com/contactsdemo/ui/activities/NewContactActivity.java
+++ app/src/main/java/manoj/jek/go/com/contactsdemo/ui/activities/NewContactActivity.java
@@ -1,6 +1,15 @@
 package manoj.jek.go.com.contactsdemo.ui.activities;
 
+import android.content.ComponentName;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +17,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,6 +48,11 @@ public class NewContactActivity extends AppCompatActivity {
     @BindView(R.id.new_contact_save)
     Button _saveButton;
 
+    private Uri _imageUri;
+    private Bitmap _bitmap;
+
+    private static final int PICTURE_REQUEST_CODE = 101;
+
     private enum PHOTO_OPTIONS {
         CAMERA, GALLERY;
     }
@@ -51,7 +74,7 @@ public class NewContactActivity extends AppCompatActivity {
         _pictureLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showImageChooserDialog();
+                openImageIntent();
             }
         });
     }
@@ -67,28 +90,80 @@ public class NewContactActivity extends AppCompatActivity {
         });
     }
 
-    private void showImageChooserDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        final CharSequence [] arr = new CharSequence[] {PHOTO_OPTIONS.CAMERA.name(), PHOTO_OPTIONS.GALLERY.name()};
-        builder.setItems(arr, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-                firePhotoPickerIntent(arr[i].toString());
-            }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.setTitle("Upload image");
-        dialog.setMessage("Take an image or choose an image from the gallery");
+    private void openImageIntent() {
+
+        // Determine Uri of camera image to save.
+        final File root = new File(Environment.getExternalStorageDirectory() + File.separator + "MyDir" + File.separator);
+        root.mkdirs();
+        final String fname = "temp.jpg";
+        final File sdImageMainDirectory = new File(root, fname);
+        _imageUri = Uri.fromFile(sdImageMainDirectory);
+
+        // Camera.
+        final List<Intent> cameraIntents = new ArrayList<Intent>();
+        final Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        final PackageManager packageManager = getPackageManager();
+        final List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
+        for(ResolveInfo res : listCam) {
+            final String packageName = res.activityInfo.packageName;
+            final Intent intent = new Intent(captureIntent);
+            intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            intent.setPackage(packageName);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, _imageUri);
+            cameraIntents.add(intent);
+        }
+
+        // Filesystem.
+        final Intent galleryIntent = new Intent();
+        galleryIntent.setType("image/*");
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+        // Chooser of filesystem options.
+        final Intent chooserIntent = Intent.createChooser(galleryIntent, "Select Source");
+
+        // Add the camera options.
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray(new Parcelable[cameraIntents.size()]));
+
+        startActivityForResult(chooserIntent, PICTURE_REQUEST_CODE);
     }
 
-    private void firePhotoPickerIntent(String optionName) {
-        PHOTO_OPTIONS option = PHOTO_OPTIONS.valueOf(optionName);
-        if(option.equals(PHOTO_OPTIONS.CAMERA)) {
-
-        } else {
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PICTURE_REQUEST_CODE) {
+                final boolean isCamera;
+                if (data == null) {
+                    isCamera = true;
+                } else {
+                    final String action = data.getAction();
+                    if (action == null) {
+                        isCamera = false;
+                    } else {
+                        isCamera = action.equals(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    }
+                }
+                if (!isCamera) {
+                    _imageUri = data.getData();
+                }
+            }
+            loadImage();
         }
+    }
+
+    private void loadImage() {
+        Glide.with(this).load(_imageUri).asBitmap().listener(new RequestListener<Uri, Bitmap>() {
+            @Override
+            public boolean onException(Exception e, Uri model, Target<Bitmap> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Bitmap resource, Uri model, Target<Bitmap> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                _pictureView.setImageBitmap(resource);
+                _bitmap = resource;
+                return true;
+            }
+        });
     }
 
     private boolean validate() {
